8dcbc61ff3fc95146334c8330e95987b8a3b008d
GSH <brian.baur.pro@gmail.com>
2025-11-03 16:30:11 +0100

    Adding square and triangle bonhomme

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ document.addEventListener("mousemove", (e) => {
   const PUPILS = document.querySelectorAll(".pupil");
   PUPILS.forEach(pupil => {
     const MAX_DISTANCE = 10;
+    const ANGLE = 100;
 
     let eye = pupil.parentElement;
     let rect = eye.getBoundingClientRect();
-    let angleX = e.clientX - rect.left ;
-    let angleY = e.clientY - rect.top + rect.height / 2;
+    let angleX = e.clientX - rect.left - 15;
+    let angleY = e.clientY - rect.top;
 
     let distance = Math.min(MAX_DISTANCE, Math.hypot(angleX, angleY) / MAX_DISTANCE);
-    let dx = Math.max(- MAX_DISTANCE, Math.min(MAX_DISTANCE, (angleX / 50) * distance));
-    let dy = Math.max(- MAX_DISTANCE, Math.min(MAX_DISTANCE, (angleY / 50) * distance));
+    let dx = Math.max(- MAX_DISTANCE, Math.min(MAX_DISTANCE, (angleX / ANGLE) * distance));
+    let dy = Math.max(- MAX_DISTANCE, Math.min(MAX_DISTANCE, (angleY / ANGLE) * distance));
     pupil.style.transform = `translate(${dx}px, ${dy}px)`;
   });
 });
